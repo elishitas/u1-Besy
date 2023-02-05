6033d9f803ba2a623eacbd31608edd82a642e611
Elishitas <arayamariaelisa@gmail.com>
2023-02-05 13:18:27 -0300

    postman

--- bootcamp-spring-boot/src/main/java/com/besysfot/bootcampspringboot/dominio/Persona.java
+++ bootcamp-spring-boot/src/main/java/com/besysfot/bootcampspringboot/dominio/Persona.java
@@ -9,14 +9,18 @@ public class Persona {
         alt + Insert
         Para generar constructores, getter, setter, toString, etc
      */
+
+    //Constructor vacio
     public Persona() {
     }
 
+    //Constructor Inicializado
     public Persona(String nombre, String apellido) {
         this.nombre = nombre;
         this.apellido = apellido;
     }
 
+    //Getter y Setters
     public String getNombre() {
         return nombre;
     }
@@ -33,6 +37,7 @@ public class Persona {
         this.apellido = apellido;
     }
 
+    //Override permite redefinar la representación de texto de un objeto
     @Override
     public String toString() {
         return "Persona{" +
